96303191597668124b116c8580c12575fcd4999a
752365469 <752365469@qq.com>
2020-06-05 16:53:25 +0800

    test

---
+++ src/main/java/com/tlh/App.java
@@ -0,0 +1,15 @@
+package com.tlh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("初始化");
+        System.out.println( "Hello World!" );
+    }
+}
